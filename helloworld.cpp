dc5c0522000c06d1cbcc6360d997e46d588d6f3c
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2022-11-17 10:32:14 -0500

    saved both strings into one file

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,10 +4,7 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     cout << "Main file! We've built it!" << endl;
-=======
     cout << "Hello World" << endl;
     cout << "I've added a change! We'll call this branch: apple" << endl;
->>>>>>> apple
 }
