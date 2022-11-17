02102a7e9492cd44844bd0b7c12d80e178b1f5be
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2022-11-17 10:34:23 -0500

    added age input

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,4 +6,8 @@ int main()
 {
     cout << "Hello World" << endl;
     cout << "I've added a change! We'll call this branch: apple" << endl;
+    int age;
+    cout << "Input your age: ";
+    cin >> age;
+    cout << "You are " << age << " years old.";
 }
