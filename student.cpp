32e7b42a9be102644e79df70dbf487e99b6e4010
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2022-11-26 11:43:01 -0500

    Added constructor for student class

--- student.cpp
+++ student.cpp
@@ -6,5 +6,12 @@ using std::cin;
 
 
 Student::Student(int id, string first_name, string last_name, int age) {
-    
-}
+    Student::id = id;
+    Student::first_name = first_name;
+    Student::last_name = last_name;
+    Student::age = age;
+    cout << "Constructed a student!" << std::endl;
+    cout << "ID: " << id << std::endl;
+    cout << "Name: " << first_name << " " << last_name << std::endl;
+    cout << "Age: " << age << std::endl;
+
--- student.hpp
+++ student.hpp
@@ -13,7 +13,6 @@ private:
     int age;
 public:
     Student(int new_id, string first_name, string last_name, int age);
-    int set_id(int new_id) {id = new_id;}
-    int get_id() {return id;} 
-
+    int set_id(int new_value) { id = new_value; }
+    int get_id() { return id; }
 };
